09324a1bceb27f5110481bbc783bf23b7833f9c0
Alexis Pavon <themaster034@gmail.com>
2022-08-31 08:36:41 -0300

    gmail services finished

---
+++ build/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
+const mail_routes_1 = __importDefault(require("./routes/mail.routes"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use(mail_routes_1.default);
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+exports.default = app;
