6bc36a7d68e447fb3212a13c14fdca295a420998
Alexis Pavon <themaster034@gmail.com>
2022-08-31 07:53:34 -0300

    set config

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,10 @@ dotenv.config()
 
 export const config = {
     PORT: process.env.PORT || 3001,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLEINT_SECRET: process.env.CLEINT_SECRET,
+    REDIRECT_URI: process.env.REDIRECT_URI,
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+    MONGO_DATABASE: process.env.DATABASE_URL || 'localhost',
+    OWNER_USER_EMAIL: process.env.OWNER_USER_EMAIL,
 }
