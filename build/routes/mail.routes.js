1cf704f1cc2a6609225cf187a1621315f56311d3
Alexis Pavon <themaster034@gmail.com>
2022-08-31 22:24:49 -0300

    front-end finished

--- build/routes/mail.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const emailValidator_1 = require("../validators/emailValidator");
-const route = (0, express_1.Router)();
-route.post('/api/sendmail', emailValidator_1.emailValidator, controllers_1.sendMail);
-exports.default = route;
