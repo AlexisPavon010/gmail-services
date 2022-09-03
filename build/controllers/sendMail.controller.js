0a1db7e7854a2698be2f5026d59a51c15a2512f6
Alexis Pavon <themaster034@gmail.com>
2022-09-03 00:22:21 -0300

    fix features

--- build/controllers/sendMail.controller.js
+++ build/controllers/sendMail.controller.js
@@ -21,48 +21,44 @@ const config_1 = require("../config");
 const promoDiscount_1 = __importDefault(require("../models/promoDiscount"));
 const oAuth2ClientServices_1 = require("../services/oAuth2ClientServices");
 const { CLIENT_ID, CLEINT_SECRET, REFRESH_TOKEN, OWNER_USER_EMAIL } = config_1.config;
-const sendMail = (req, res) => {
+const sendMail = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { email } = req.body;
-    const filePath = path_1.default.join(__dirname, '../public/example.html');
-    const source = fs_1.default.readFileSync(filePath, 'utf-8').toString();
-    const template = handlebars_1.default.compile(source);
-    const replacements = {
-        username: "Darth Vader"
-    };
-    const htmlToSend = template(replacements);
-    function sendMail() {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                const accessToken = yield oAuth2ClientServices_1.oAuth2Client.getAccessToken();
-                const emailModel = new promoDiscount_1.default({ email });
-                const emailSaved = yield emailModel.save();
-                const transport = nodemailer_1.default.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        type: 'OAuth2',
-                        user: OWNER_USER_EMAIL,
-                        clientId: CLIENT_ID,
-                        clientSecret: CLEINT_SECRET,
-                        refreshToken: REFRESH_TOKEN,
-                        accessToken: accessToken,
-                    },
-                });
-                const mailOptions = {
-                    from: 'test@test123123.com',
-                    to: email,
-                    subject: 'Hello from gmail using API',
-                    text: 'Hello from gmail email using API',
-                    html: htmlToSend
-                };
-                return yield transport.sendMail(mailOptions);
-            }
-            catch (error) {
-                return error;
-            }
+    try {
+        const filePath = path_1.default.join(__dirname, '../public/example.html');
+        const source = fs_1.default.readFileSync(filePath, 'utf-8').toString();
+        const template = handlebars_1.default.compile(source);
+        const replacements = {
+            username: "Darth Vader"
+        };
+        const htmlToSend = template(replacements);
+        const accessToken = yield oAuth2ClientServices_1.oAuth2Client.getAccessToken();
+        const emailModel = new promoDiscount_1.default({ email });
+        const emailSaved = yield emailModel.save();
+        const transport = nodemailer_1.default.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: OWNER_USER_EMAIL,
+                clientId: CLIENT_ID,
+                clientSecret: CLEINT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
         });
+        const mailOptions = {
+            from: 'test@test123123.com',
+            to: email,
+            subject: 'Hello from gmail using API',
+            text: 'Hello from gmail email using API',
+            html: htmlToSend
+        };
+        const response = yield transport.sendMail(mailOptions);
+        console.log(response);
+        res.status(200).send({ ok: true });
     }
-    sendMail()
-        .then((result) => res.status(200).send({ result }))
-        .catch((error) => res.status(404).send(error.message));
-};
+    catch (error) {
+        console.log(`<------${error}------>`);
+        res.status(400).send(error);
+    }
+});
 exports.sendMail = sendMail;
--- src/controllers/sendMail.controller.ts
+++ src/controllers/sendMail.controller.ts
@@ -7,54 +7,53 @@ import fs from 'fs'
 import { config } from "../config";
 import promoModel from '../models/promoDiscount'
 import { oAuth2Client } from "../services/oAuth2ClientServices";
+import { clouddebugger } from "googleapis/build/src/apis/clouddebugger";
 const { CLIENT_ID, CLEINT_SECRET, REFRESH_TOKEN, OWNER_USER_EMAIL } = config;
 
-export const sendMail: RequestHandler = (req, res) => {
+export const sendMail: RequestHandler = async (req, res) => {
     const { email } = req.body
 
+    try {
 
-    const filePath = path.join(__dirname, '../public/example.html');
-    const source = fs.readFileSync(filePath, 'utf-8').toString();
-    const template = handlebars.compile(source);
-    const replacements = {
-        username: "Darth Vader"
-    };
-    const htmlToSend = template(replacements);
+        const filePath = path.join(__dirname, '../public/example.html');
+        const source = fs.readFileSync(filePath, 'utf-8').toString();
+        const template = handlebars.compile(source);
+        const replacements = {
+            username: "Darth Vader"
+        };
+        const htmlToSend = template(replacements);
 
-    async function sendMail() {
-        try {
-            const accessToken: any = await oAuth2Client.getAccessToken();
-            const emailModel = new promoModel({ email })
-            const emailSaved = await emailModel.save()
+        const accessToken: any = await oAuth2Client.getAccessToken();
+        const emailModel = new promoModel({ email })
+        const emailSaved = await emailModel.save()
 
-            const transport = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    type: 'OAuth2',
-                    user: OWNER_USER_EMAIL,
-                    clientId: CLIENT_ID,
-                    clientSecret: CLEINT_SECRET,
-                    refreshToken: REFRESH_TOKEN,
-                    accessToken: accessToken,
-                },
-            });
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: OWNER_USER_EMAIL,
+                clientId: CLIENT_ID,
+                clientSecret: CLEINT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
+        });
 
-            const mailOptions = {
-                from: 'test@test123123.com',
-                to: email,
-                subject: 'Hello from gmail using API',
-                text: 'Hello from gmail email using API',
-                html: htmlToSend
-            };
+        const mailOptions = {
+            from: 'test@test123123.com',
+            to: email,
+            subject: 'Hello from gmail using API',
+            text: 'Hello from gmail email using API',
+            html: htmlToSend
+        };
 
-            return await transport.sendMail(mailOptions);
-        } catch (error) {
-            return error;
-        }
+        const response = await transport.sendMail(mailOptions);
+        console.log(response)
+
+        res.status(200).send({ ok: true })
+    } catch (error:any) {
+        console.log(`<------${error}------>`)
+        res.status(400).send(error)
     }
-
-    sendMail()
-        .then((result) => res.status(200).send({ result }))
-        .catch((error) => res.status(404).send(error.message));
 
 }
